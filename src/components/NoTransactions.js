8731925b965db4e7a40407efa1481b689196f796
Your Name <umeshsgowda770@gmail.com>
2024-12-24 11:17:44 +0530

    bug fixed

--- src/components/NoTransactions.js
+++ src/components/NoTransactions.js
@@ -4,7 +4,7 @@ import transactions from "../assets/transactions.svg"
 function NoTransactions() {
   return (
     <div className='flex justify-center items-center w-full flex-col mb-8'>
-        <img src={transactions}
+        <img src={transactions} alt='no transactions'
         className='w-[200px]  md:w-[400px] m-14'></img>
         <p className='text-[1rem] md:text-[1.25rem] font-montserrat tracking-widest'>You Have No Transactions Currently</p></div>
   )
